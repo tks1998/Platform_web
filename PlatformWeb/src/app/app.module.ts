6758775d775aeadeedaca74c5e9dc98f889933b9
Ry Sama <conlocxoaythuytrieu@gmail.com>
2019-08-03 11:20:44 +0700

    Start project

--- PlatformWeb/src/app/app.module.ts
+++ PlatformWeb/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
