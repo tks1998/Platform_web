1bccd907e20ade5972dab199954ffac572317341
Ry Sama <conlocxoaythuytrieu@gmail.com>
2019-08-05 14:09:51 +0700

    fix ..

--- PlatformWeb/src/app/app-routing.module.ts
+++ PlatformWeb/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { StoreComponent } from './store/store.component'
+import { MainContentComponent } from './main-content/main-content.component'
 
 const routes: Routes = [
   {
     path: 'store',
     component: StoreComponent
+  },
+  {
+    path: '',
+    component: MainContentComponent
   }
 ];
 
