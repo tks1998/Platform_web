adea4e1be13ccbb1b032e45838f9d796e5253f54
xuantruongdang <xuantruongdang.it@gmail.com>
2019-08-07 13:18:44 +0700

    add smootj scroll categories

--- PlatformWeb/src/app/store/store.component.ts
+++ PlatformWeb/src/app/store/store.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-store',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private location: Location) {
+  }
+  currentUrl()
+  {
+    return this.location.path();
+  }
   ngOnInit() {
   }
 }
